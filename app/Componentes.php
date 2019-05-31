d997f42d286cf487a84a9fc71c63f5a99412b33e
jfernandez62 <jfernandez62@misena.edu.co>
2019-05-30 20:15:04 -0500

    Create Componentes.php

---
+++ app/Componentes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Componentes extends Model
+{
+    protected $table = "componentes";
+    protected $primarykey = "Idcomp";
+    public $timestamps = false;
+    protected $fillable =[
+        'Idcomp',
+        'Nombrecomp',
+        'Idtipocomp'
+        ];
+}
